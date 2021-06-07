bf61d720fb5251447a4e5cff7267d06e9c9338cf
rafa <rafa@gavisa.es>
2021-06-07 17:22:23 +0200

    Little fix

--- app/src/main/java/com/example/coordinatorquestion/ScrollingActivity.kt
+++ app/src/main/java/com/example/coordinatorquestion/ScrollingActivity.kt
@@ -9,6 +9,7 @@ class ScrollingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(findViewById(R.id.toolbar))
         recycler_view.adapter = PostAdapter(arrayListOf())
     }
 
